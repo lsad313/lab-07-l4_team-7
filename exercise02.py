755e53f7ed2cfc672037ba6a010dddac636c74c1
tula487 <80795221+tula487@users.noreply.github.com>
2021-04-27 13:42:51 +1200

    Update exercise02.py

--- exercise02.py
+++ exercise02.py
@@ -6,19 +6,19 @@ datetime_object = datetime.datetime.now()
 print("Current date and time: ", datetime_object)
 
 # modify the following print statements using datetime_object appropriately
-print("Current year: ", datetime_object)
-print("Current month: ", datetime_object)
-print("Current day: ", datetime_object)
-print("Current time: ", datetime_object)
+print("Current year: ", datetime_object.year)
+print("Current month: ", (datetime_object.strftime("%B")))
+print("Current day: ", (datetime_object.strftime("%A")))
+print("Current time: ", (datetime_object.strftime("%X")))
 
 # modify the following statement that creates another datetime_object.
 # the second datetime_object contains information about yesterday
-datetime_object2 = datetime_object
+datetime_object2 = datetime_object - datetime.timedelta(days=1)
 
 # print yesterday's date and time
 print("Yesterday's date and time: ", datetime_object2)
 
 # modify the following statement to print the current date and time in
 # dd-mm-YY H:M:S format
-datetime_object3 = datetime_object
+datetime_object3 = datetime_object.strftime('%d-%m-%y %H:%M:%S')
 print("Current date and time: ", datetime_object3)
