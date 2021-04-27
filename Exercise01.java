030cbfa2a033dc14687e9b3a83acda5ad5edbe83
Pjiv657 <54612423+Pjiv657@users.noreply.github.com>
2021-04-27 13:16:51 +1200

    completed

--- Exercise01.java
+++ Exercise01.java
@@ -14,25 +14,25 @@ public class ExerciseOne {
     
     // Complete this method to return the word in all uppercase
     private String stringToUppercase(String word) {
-        return word;
+        return word.toUpperCase();
     }
     
     // Complete this method to return the length of the word
     private int wordLength(String word) {
-        return 0;
+        return word.length();
     }
     
     // Complete this method to return the index of 
     // the specified substring in the given word
     private int stringIndexOf(String subStr, String word) {
-        return 0;
+        return word.indexOf(subStr);
     }
     
     // Complete this method to return a combination of 
     // the first three letters and the last four letters of the given word.
     // E.g. given the word "redorangeyellowgreen", "redgreen" should be returned
     private String wordCombo(String word) {
-        return word;
+        return word.substring(0, 3) + word.substring(word.length() - 5, word.length());
     }
     
     // Complete this method to return the frequencey of 
@@ -47,7 +47,9 @@ public class ExerciseOne {
         // if the character is the same as the given letter,
         // then add 1 to freq
         for (int i = 0; i < word.length(); i++) {
-            
+            if (word.charAt(i) == letter){
+             freq++;   
+            }
         }
         return freq;
     }
